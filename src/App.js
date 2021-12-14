18acf8eacc3705902cd163e797e280a5244be758
DamienLKT <damienlim82@gmail.com>
2021-12-15 04:20:00 +0800

    lesson 3-7 JS Lists and Keys

--- src/App.js
+++ src/App.js
@@ -1,21 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from './Header';
+import Content from './Content';
+import Footer from './Footer';
 
-function App() {
-  // const name="Damien";
-  const handleNameChange = () => {
-    const names = ['Bob','Dave','Kevin','Damien'];
-    const int = Math.floor(Math.random()*4);
-    return names[int];
-  }
+function App() {  
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />        
-        <p>
-          Hello {handleNameChange()}!
-        </p>
-      </header>
+      <Header />
+      <Content />
+      <Footer />
     </div>
   );
 }
---
+++ src/Header.js
@@ -0,0 +1,11 @@
+const Header = () => {
+    
+
+    return (
+        <header>
+           <h1>Groceries List</h1>             
+        </header>
+    )
+}
+
+export default Header;
